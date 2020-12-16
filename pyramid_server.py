473ceb690f2f06543dda8e29d0b3c7c79f80609f
Abhishek Agnihotri <agni.abhi00@gmail.com>
2020-12-17 00:18:40 +0530

    get response from pyramid server and display in webpage

--- pyramid_server.py
+++ pyramid_server.py
@@ -1,9 +1,22 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+from pyramid.events import NewRequest
 from pyramid.view import view_config
 import model
 import json
+
+def add_cors_headers_response_callback(event):
+    def cors_headers(request, response):
+        response.headers.update({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Max-Age': '1728000',
+        })
+    event.request.add_response_callback(cors_headers)
+
 
 print('init model')
 model = model.Model()
@@ -11,10 +24,10 @@ model = model.Model()
 @view_config(
     route_name='get_data',
     renderer='json',
-    request_method='GET'
+    request_method='POST'
 )
 def get_data(request):
-    print(request.body)
+    print(request)
     data = json.loads(request.body)
     print('text:  ', data['text'])
     out = model.predict(str(data['text']))
@@ -22,6 +35,7 @@ def get_data(request):
 
 if __name__ == '__main__':
     with Configurator() as config:
+        config.add_subscriber(add_cors_headers_response_callback, NewRequest)
         config.add_route('get_data', '/get_data')
         config.scan()
         app = config.make_wsgi_app()
--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,11 +1,46 @@
+import React from "react";
 import './App.css';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  const [text, setText] = React.useState("");
+  const [output,setOutput] = React.useState("");
+
+  function setOut(t){
+    setOutput(t);
+  }
+
+  const handleSubmit = async(event) => {
+    const j_data = {
+      text
+    }
+    axios.post(
+      "http://127.0.0.1:6543/get_data", 
+       JSON.stringify(j_data)
+      ).then(resp => {
+        setOut(resp.data.output);
+    });
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      hi
+      <div className='form-container'>
+        <form onSubmit={handleSubmit}>
+          <h1>Sentiment Analysis</h1>
+            <textarea
+              name="text"
+              type="text"
+              value={text}
+              onChange={e => setText(e.target.value)}
+              placeholder='text'
+              required />
+          <div className={output}>
+            {output}
+          </div>
+          <button>Check Sentiment</button>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
